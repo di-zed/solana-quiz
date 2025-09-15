3641f6c24df604cb2e1f2c7a49a2085147e38424
Dima Zaichenko <dmitriy@pearlconvert.no>
2025-09-15 09:29:37 +0300

    Updated Front container

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -5,9 +5,8 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
+          src="/solana.svg"
+          alt="Solana"
           width={180}
           height={38}
           priority
@@ -16,7 +15,7 @@ export default function Home() {
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
+              app/page.tsx
             </code>
             .
           </li>
@@ -33,13 +32,12 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
+              src="/phantom.svg"
+              alt="Login"
               width={20}
               height={20}
             />
-            Deploy now
+            Login
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
@@ -84,7 +82,7 @@ export default function Home() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://solana.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -95,7 +93,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to solana.com →
         </a>
       </footer>
     </div>
